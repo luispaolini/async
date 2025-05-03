22caacbc3b7caf2675e33370aafa4f741de77ea8
Luis Paolini <luis.paolini@gmail.com>
2025-05-03 09:25:09 -0300

    Initial setup

---
+++ async/src/test/java/dev/luispaolini/async/AsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luispaolini.async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
